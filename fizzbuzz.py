4cd2bdff609cf13c5e24576460712d0381c0e651
Max Lane <lmax20@vt.edu>
2023-08-25 16:15:37 -0400

    fixed fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,7 +2,7 @@ for val in range(1, 21):
     if (val % 5 == 0 and val % 3 == 0):
         print("FizzBuzz")
     else if (val % 3 == 0):
-        print("Fuzz")
+        print("Fizz")
     else if (val % 5 == 0):
         print("Buzz")
     else:
